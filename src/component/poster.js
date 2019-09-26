69cdf164908aabc85a6b32bec859fa97c94cf370
Kyle Keck <keckkyle@gmail.com>
2019-09-26 18:46:57 +0700

    update poster urls

--- src/component/poster.js
+++ src/component/poster.js
@@ -1,23 +1,5 @@
 import React from 'react';
 
-// const Poster = (props) => {
-//   let poster = ''
-//   for(var i = 0; i < props.images.length; i++){
-//     if(props.images[i].type === "POSTER"){
-//       poster = props.images[i].url
-//       // let url = props.images[i].url
-//       // if(url[url.length-1] === "?"){
-//       //   poster = url.substr(0, url.length-1)
-//       // }
-//     }
-//   }
-
-//   return(
-//       <img src={poster} alt="Movie Poster"/>
-//   )
-// }
-
-
 class Poster extends React.Component {
   state = {
     poster: '',
